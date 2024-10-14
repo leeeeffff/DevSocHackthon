23d58dfb5e3483652891c7b771d6bd6c1298f827
leeeeffff <krleepi@gmail.com>
2024-10-14 15:17:49 +1100

    added some sht

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landingpage from './pages/Landingpage';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" exact Component={ Landingpage } />
         </Routes>
