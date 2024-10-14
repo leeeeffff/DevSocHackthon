5e3dc8838aaf26094f4f2956934051b14464adc8
Brian <long.dang@student.unsw.edu.au>
2024-10-14 16:54:07 +1100

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landingpage from './pages/Landingpage';
+import AboutPage from './pages/AboutPage';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
         <Routes>
-          <Route path="/" exact Component={ Landingpage } />
+
+          <Route path="/" exact Component={ AboutPage } />
         </Routes>
       </Router>
     </div>
