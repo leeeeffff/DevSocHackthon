0195c6120f13bba4c3d36742451afa55b85d9571
vitto <z5404184@ad.unsw.edu.au>
2024-10-14 16:52:40 +1100

    Added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landingpage from './pages/Landingpage';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
         <Routes>
-          <Route path="/" exact Component={ Landingpage } />
+          <Route path="/" exact Component={ Login } />
         </Routes>
       </Router>
     </div>
