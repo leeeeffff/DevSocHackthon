8c426e17f21d2458b95f04fbce1da7b02fe9f762
Brian <long.dang@student.unsw.edu.au>
2024-10-16 01:37:33 +1100

    updated AboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landingpage from './pages/Landingpage';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import SignUp from './pages/SignUp';
+import AboutPage from './pages/AboutPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/AboutPage" exact Component={ AboutPage } />
           <Route path="/" exact Component={ Landingpage } />
           <Route path="/login" exact Component={ Login } />
           <Route path="/SignUp" exact Component={ SignUp } />
