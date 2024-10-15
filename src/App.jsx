09dc358cf84074a37a0add3a7c1829d849e77cae
Brian <long.dang@student.unsw.edu.au>
2024-10-16 00:21:53 +1100

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landingpage from './pages/Landingpage';
 import AboutPage from './pages/AboutPage';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
         <Routes>
-
-          <Route path="/" exact Component={ AboutPage } />
+          <Route path="/" exact Component={ Landingpage } />
+          <Route path="/login" exact Component={ Login } />
+          <Route path="/aboutPage" exact Component={ AboutPage } />
         </Routes>
       </Router>
     </div>
