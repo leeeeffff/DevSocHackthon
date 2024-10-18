2489689330bba3d97fe26aef94bfc6372a4255ae
vitto <z5404184@ad.unsw.edu.au>
2024-10-19 06:53:20 +1100

    Corrected Link to Profile Page AA

--- src/components/Navbaradvice.jsx
+++ src/components/Navbaradvice.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
           <img src={Profile} alt="Profile" className="profile-picture" />
           {dropdownOpen && (
             <div className="dropdown-menu">
-              <Link to="/profile" className="dropdown-item">Profile</Link>
+              <Link to="/ProfilePage" className="dropdown-item">Profile</Link>
               <Link to="/settings" className="dropdown-item">Settings</Link>
               <Link to="/logout" className="dropdown-item">Log Out</Link>
             </div>
